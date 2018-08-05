0b1f8b7f1bfc6819e0c3bfb5564bc9822301a484
EC2 Default User <ec2-user@ip-172-31-0-59.ap-southeast-1.compute.internal>
2018-08-05 12:32:14 +0000

    2018.08.05-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ApplicationRecord
   has_many :scores, dependent: :destroy, autosave: true
   has_many :store_users, dependent: :destroy
   has_many :stores, through: :store_users
-    
+
   attr_accessor :login
-  
+
   # validates :username,
   # uniqueness: { case_sensitive: :false },
   # length: { minimum: 3, maximum: 20 },
   # format: { with: /\A[a-z0-9]+\z/, message: "ユーザー名は半角英数字です"}
-  
-  extend Enumerize
-  enumerize :gender, in: [:unknown, :male, :female], default: :unknown, scope: true
-  
+
+  # extend Enumerize
+  # enumerize :gender, in: [:unknown, :male, :female], default: :unknown, scope: true
+
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
@@ -22,11 +22,11 @@ class User < ApplicationRecord
       where(conditions).first
     end
   end
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable#, :confirmable, :authentication_keys => [:login]
   validates :name,  presence: true, length: { maximum: 50 }
-  
+
 end
